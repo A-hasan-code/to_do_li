9dc462f240b051f492f8c14a2ef83a6c094d92f9
A-hasan-code <alihasan12soft786@gmail.com>
2025-07-29 11:40:20 +0500

    theme

---
+++ themecontext/src/Context/Theme.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
+console.log("ThemeContext", ThemeContext)
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
